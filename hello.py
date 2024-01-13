129b99e178e157f48b8b79e0aedd978703726156
Geele Mohamed <GeeleM7@gmail.com>
2024-01-13 13:22:24 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+first_name = "Geele"
+last_name = "Mohamed"
+full_name = first_name + " " + last_name
+print(full_name)
+
+num1 = 20;
+num2 = 40;
+result = num1 + num2;
+print(result)
---
+++ meaning.py
@@ -0,0 +1,8 @@
+meaning = 8
+
+print('')
+
+if meaning > 10:
+    print('Right on!')
+else:
+    print('Not today')
